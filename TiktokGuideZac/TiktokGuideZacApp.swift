d8361d6f0d60c29f92045a96aad997eb12d1f54f
MaurZac <maur.mx@gmail.com>
2024-06-20 11:58:07 -0600

    Initial Commit

---
+++ TiktokGuideZac/TiktokGuideZacApp.swift
@@ -0,0 +1,17 @@
+//
+//  TiktokGuideZacApp.swift
+//  TiktokGuideZac
+//
+//  Created by Mauricio Zarate Chula on 6/20/24.
+//
+
+import SwiftUI
+
+@main
+struct TiktokGuideZacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
