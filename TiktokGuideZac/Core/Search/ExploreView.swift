f320daf063046754d91637686c0aa20f72f7c263
MaurZac <maur.mx@gmail.com>
2024-06-20 20:37:16 -0600

    FeedView/FeedCell and TabBar created

---
+++ TiktokGuideZac/Core/Search/ExploreView.swift
@@ -0,0 +1,18 @@
+//
+//  ExploreView.swift
+//  TiktokGuideZac
+//
+//  Created by Mauricio Zarate Chula on 6/20/24.
+//
+
+import SwiftUI
+
+struct ExploreView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExploreView()
+}
